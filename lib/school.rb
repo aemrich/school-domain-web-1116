fd21216708a5bf2f6a0432aca4d8eefb2167ff04
aemrich <adam.emrich@gmail.com>
2016-11-30 17:49:26 -0500

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name
+    @roster ={}
+  end
+
+  def add_student(name, grade)
+    roster[grade]||=[]
+    roster[grade]<<name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+      reformed_hash ={}
+      array = @roster.sort_by {|grade, name| grade}
+
+      array.each do |key, value|
+        reformed_hash[key] = value.sort
+      end
+    reformed_hash
+  end
+end
